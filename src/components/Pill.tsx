fed9b7a1f6d2fdf367a62ddb5cfd82ca1aae5421
Jason Chiang <chiangjason19@gmail.com>
2024-03-12 13:27:48 -0700

    Ran biome format --write.. Biomejs is a cool tech

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,36 +1,44 @@
-import clsx from 'clsx'
+import clsx from "clsx";
 import { FunctionComponent } from "preact";
 
-import styles from '../styles/pill.module.css'
+import styles from "../styles/pill.module.css";
 
 interface PillProps {
-  title: string
-  description: string
-  logo?: string
-  logoAlt?: string
-  dates?: string
-  url?: string
+	title: string;
+	description: string;
+	logo?: string;
+	logoAlt?: string;
+	dates?: string;
+	url?: string;
 }
 
-const Pill: FunctionComponent<PillProps> = ({ logo, logoAlt, description, title, dates, url }) => {
-  return (
-    <a 
-      href={url} target="_blank" 
-      rel="noreferrer" 
-      className={clsx(styles.pillLink, url && styles.pillLinkHover)}
-    >
-      <div className={styles.pillContainer}>
-        <img className={styles.logo} src={logo} alt={logoAlt}/>
-        <div className={styles.details}>
-          <h3 className={styles.title}>{title}</h3>
-          <div className={styles.pillDescriptionContainer}>
-            <p className={styles.role}>{description}</p>
-            <p className={styles.dates}>{dates}</p>
-          </div>
-        </div>
-      </div>
-    </a>
-  )
-}
+const Pill: FunctionComponent<PillProps> = ({
+	logo,
+	logoAlt,
+	description,
+	title,
+	dates,
+	url,
+}) => {
+	return (
+		<a
+			href={url}
+			target="_blank"
+			rel="noreferrer"
+			className={clsx(styles.pillLink, url && styles.pillLinkHover)}
+		>
+			<div className={styles.pillContainer}>
+				<img className={styles.logo} src={logo} alt={logoAlt} />
+				<div className={styles.details}>
+					<h3 className={styles.title}>{title}</h3>
+					<div className={styles.pillDescriptionContainer}>
+						<p className={styles.role}>{description}</p>
+						<p className={styles.dates}>{dates}</p>
+					</div>
+				</div>
+			</div>
+		</a>
+	);
+};
 
-export default Pill
+export default Pill;
